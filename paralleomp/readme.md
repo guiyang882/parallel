6f5e6ea6144d2f183c158826a5eb6a44bb7c9f60
liuguiyangnwpu <liuguiyangnwpu@163.com>
2017-03-06 20:11:30 +0800

    添加threadpool程序

---
+++ paralleomp/readme.md
@@ -0,0 +1,40 @@
+## 并行化OMP算法
+
+### 在未优化前，测试情况如下：
+```bash
+➜  bin git:(master) ✗ time ./testsparse 
+During loadDicts 1324.19 ms !
+Dict Size is 24000 * 128
+Detect Image Feature size is 896 * 128
+24, 26, 39, 10, 37, 1, 22, 17, 20, 13, 31, 12, 14, 25, 30, 40, 4, 41, 36, 42, 19, 8, 43, 44, 27, 34, 35, 18, 0, 11, 29, 47, 16, 46, 32, 7, 3, 9, 33, 23, 28, 6, 38, 21, 5, 45, 2, 
+During SRClassify 99108.1 ms !
+./testsparse  100.25s user 0.32s system 99% cpu 1:41.13 total
+```
+
+### 在mac中使用openmp进行优化
+
+**cpu time: 100s ---> 55.579s**
+
+```bash
+➜  cmake-build-debug git:(master) ✗ time ./testsparse
+During loadDicts 1844.42 ms !
+Dict Size is 24000 * 128
+Detect Image Feature size is 896 * 128
+24, 26, 39, 10, 37, 1, 22, 17, 20, 13, 31, 12, 14, 25, 30, 40, 4, 41, 36, 42, 19, 8, 43, 44, 27, 34, 35, 18, 0, 11, 29, 47, 16, 46, 32, 7, 3, 9, 33, 23, 28, 6, 38, 21, 5, 45, 2,
+During SRClassify 188377 ms !
+./testsparse  189.94s user 0.90s system 343% cpu 55.579 total
+```
+
+### 在Mac中使用openmp+clang-omp++配合编译器优化O3
+
+**cpu time: 55.579s ---> 12.590s**
+
+```bash
+➜  bin git:(master) ✗ time ./testsparse 
+During loadDicts 451.212 ms !
+Dict Size is 24000 * 128
+Detect Image Feature size is 896 * 128
+24, 26, 39, 10, 37, 1, 22, 17, 20, 13, 31, 12, 14, 25, 30, 40, 4, 41, 36, 42, 19, 8, 43, 44, 27, 34, 35, 18, 0, 11, 29, 47, 16, 46, 32, 7, 3, 9, 33, 23, 28, 6, 38, 21, 5, 45, 2, 
+During SRClassify 42580 ms !
+./testsparse  42.94s user 0.22s system 342% cpu 12.590 total
+```
--- paralleomp/sparserepresentationNoneTemplate.hpp
+++ paralleomp/sparserepresentationNoneTemplate.hpp
@@ -20,91 +20,6 @@
 using namespace std;
 using namespace Eigen;
 
-vector<double> operator*(vector<double> vec, double data) {
-	/*operator*			数组与数相乘
-	*vec				数组（N）
-	*data				数
-	*/
-	int len = vec.size();
-	if(!len) {
-		cerr << "operator*:eeror." << endl;
-		throw runtime_error("operator*:error");
-	}
-	vector<double> result(len, 0.0);
-	for(int i = 0; i < len; i++) {
-		result[i] = (vec[i] * data);
-	}
-	return result;
-}
-
-//数组减法
-vector<double> operator-(vector<double> vec1, vector<double> vec2) {
-	/*operator-			数组相减（vec1-vec2）
-	*vec1				数组（N）
-	*vec2				数组（N）
-	*/
-	if (!vec1.size() || !vec2.size() || vec1.size() != vec2.size()){
-		cerr << "operator-:error." << endl;
-		throw runtime_error("operator-:error");
-	}
-	int len = vec1.size();
-	vector<double> result(len, 0.0);
-	for (int i = 0; i < len; i++) {
-		result[i] = (vec1[i] - vec2[i]);
-	}
-
-	return result;
-}
-
-//数组加法
-vector<double> operator+(vector<double> vec1, vector<double> vec2) {
-	/*operator-			数组相减（vec1-vec2）
-	*vec1				数组（N）
-	*vec2				数组（N）
-	*/
-	if(!vec1.size() || !vec2.size() || vec1.size() != vec2.size()){
-		cerr << "operator+:error." << endl;
-		throw runtime_error("operator+:error");
-	}
-	int len = vec1.size();
-	vector<double> result(len, 0.0);
-	for(int i = 0; i < len; i++) {
-		result[i] = (vec1[i] + vec2[i]);
-	}
-
-	return result;
-}
-
-//矩阵与数组乘法
-vector<double> operator*(vector<vector<double>> mat, vector<double> vec) {
-	/*operator*			矩阵与数组相乘
-	*mat				矩阵（N*M）
-	*vec				数组（N）
-	*/
-	int row = mat.size();
-	if (!row || row != vec.size()) {
-		cerr << "operator*:eeror." << endl;
-		throw runtime_error("operator*:error");
-	}
-	int col = mat[0].size();
-	if (!col) {
-		cerr << "operator*:eeror." << endl;
-		throw runtime_error("operator*:error");
-	}
-
-	vector<double> result(col, 0.0);
-	double tmp;
-	for(int i = 0; i < col; i++) {
-		tmp = 0.0;
-		for(int j = 0; j < row; j++) {
-			tmp += mat[j][i] * vec[j];
-		}
-		result[i] = (tmp);
-	}
-	return result;
-}
-
-
 //"RETRIEVESPARSITY" : "9",
 //"RETRIEVEMINRESIDUAL" : "1"
 class LSparseRepresentation {
@@ -112,9 +27,9 @@ public:
 	LSparseRepresentation(vector<vector<vector<double>>> dicts);
 	~LSparseRepresentation();
 	//稀疏表示分类（一组特征）
-	int SRClassify(vector<double>& y, double min_residual, int sparsity);
+	int SRClassify(const vector<double>& y, double min_residual, int sparsity);
 	//稀疏表示分类（多组特征）
-	bool SRClassify(vector<vector<double>>& y, double min_residual, int sparsity, vector<int> &srres);
+	bool SRClassify(const vector<vector<double>>& y, double min_residual, int sparsity, vector<int> &srres);
 	
 private:
 	vector<vector<double>> dic;		//存放字典 （vector<double>：列）
@@ -123,16 +38,16 @@ private:
 
 private:
 	//两个数组内积
-	double Dot(vector<double>& vec1, vector<double>& vec2);
+	double Dot(const vector<double>& vec1, const vector<double>& vec2);
 
 	//数组2范数
-	double Norm(vector<double> vec);
+	double Norm(const vector<double>& vec);
 
 	//求解最小二乘问题
 	bool solve(vector<vector<double>>& phi, vector<double>& y, vector<double>& x);
 
 	//OMP算法
-	bool OrthMatchPursuit(vector<double>& y, double min_residual, int sparsity, vector<double>& x, vector<int>& patch_indices);
+	bool OrthMatchPursuit(const vector<double>& y, double min_residual, int sparsity, vector<double>& x, vector<int>& patch_indices);
 };
 
 //构造函数
@@ -152,7 +67,7 @@ LSparseRepresentation::LSparseRepresentation(vector<vector<vector<double>>> dict
 }
 
 //稀疏表示分类（一组特征）
-int LSparseRepresentation::SRClassify(vector<double>& y, double min_residual, int sparsity) {
+int LSparseRepresentation::SRClassify(const vector<double>& y, double min_residual, int sparsity) {
 	/*SRClassify		稀疏表示识别
 	*y					特征
 	*min_residual		最小残差
@@ -180,12 +95,8 @@ int LSparseRepresentation::SRClassify(vector<double>& y, double min_residual, in
 	for (i = 0; i < patch_indices.size(); ++i) {
 		x[patch_indices[i]] = coefficient[i];
 	}
-
-	// vector<int>().swap(patch_indices);
-	// vector<double>().swap(coefficient);
 	
 	int result = 0;//分类结果 属于字典对应的类
-
 	int start = 0;//某一类开始位置
 	double mindist = 100000000;
 	for(i = 0; i < classnum; ++i) {
@@ -197,8 +108,6 @@ int LSparseRepresentation::SRClassify(vector<double>& y, double min_residual, in
 		}
 		double dist = Norm(y - tmp);
 
-		// vector<double>().swap(tmp);
-
 		if (mindist > dist) {
 			mindist = dist;
 			result = i;
@@ -206,14 +115,13 @@ int LSparseRepresentation::SRClassify(vector<double>& y, double min_residual, in
 
 		start += dicclassnum[i];
 	}
-	// vector<double>().swap(x);
 
 	return result; //result（0，1，2，3，...）
 
 }
 
 //稀疏表示分类（多组特征）
-bool LSparseRepresentation::SRClassify(vector<vector<double>>& y,
+bool LSparseRepresentation::SRClassify(const vector<vector<double>>& y,
 												 double min_residual, int sparsity,
 												 vector<int> &srres){
 	/*SRClassify		稀疏表示识别
@@ -278,16 +186,12 @@ LSparseRepresentation::~LSparseRepresentation() {
 }
 
 //两个数组内积
-double LSparseRepresentation::Dot(vector<double>& vec1, vector<double>& vec2) {
+double LSparseRepresentation::Dot(const vector<double>& vec1, const vector<double>& vec2) {
 	/*Norm				求两个数组的内积
 	*vec1				数组（N）
 	*vec2				数组（N）
 	*return：double
 	*/
-	if(!vec1.size() || !vec2.size() || vec1.size() != vec2.size()){
-		cerr << "Dot:error." << endl;
-		throw runtime_error("Dot:error");
-	}
 	double sum = 0;
 	int len = vec1.size();
 	for(int i = 0; i < len; ++i) {
@@ -298,15 +202,11 @@ double LSparseRepresentation::Dot(vector<double>& vec1, vector<double>& vec2) {
 }
 
 //数组2范数
-double LSparseRepresentation::Norm(vector<double> vec) {
+double LSparseRepresentation::Norm(const vector<double>& vec) {
 	/*Norm				求数组的2范数
 	*vec				数组（N）
 	*return：double
 	*/
-	if (!vec.size()) {
-		cerr << "Norm:error." << endl;
-		throw runtime_error("Norm:error");
-	}
 	double norm = 0;
 	int len = vec.size();
 	for(int i = 0; i < len; ++i) {
@@ -345,7 +245,6 @@ bool LSparseRepresentation::solve(vector<vector<double>>& phi,
 }
 
 //OMP算法
-
 bool LSparseRepresentation::OrthMatchPursuit(vector<double>& y,
 												double min_residual, int sparsity,
 												vector<double>& x, vector<int>& patch_indices) {
@@ -392,9 +291,6 @@ bool LSparseRepresentation::OrthMatchPursuit(vector<double>& y,
 		x.push_back(0.0);
 
 		if( !solve(phi, y, x) ) {
-			vector<double>().swap(residual);
-			for (int i = 0; i < phi.size(); ++i) vector<double>().swap(phi[i]);
-			vector<vector<double>>().swap(phi);
 			return false;
 		}
 
@@ -405,10 +301,6 @@ bool LSparseRepresentation::OrthMatchPursuit(vector<double>& y,
 		if (x.size() >= sparsity)
 			break;
 	}
-
-	vector<double>().swap(residual);
-	for(int i = 0; i < phi.size(); ++i) vector<double>().swap(phi[i]);
-	vector<vector<double>>().swap(phi);
 
 	return true;
 }
